73729da95ebaf1e5e4a5121ca106e8dacade23c6
Ifeanyi Umeh <somto_umeh@yahoo.com>
2022-03-02 22:48:44 -0500

    Completed challenge and added  a counter underneath

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [offsetTop, setOffsetTop] = useState(300);
+  const [counter, setCounter] = useState(0);
+
+  function moveBoxUp() {
+    setOffsetTop(offsetTop - 20);
+  }
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
+  function decrement() {
+    setCounter(counter - 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Move this box!</h1>
+      <button onClick={moveBoxUp}> 👆 Move up 👆</button>
+
+      <div
+        className="box"
+        style={{ transform: `translateY(${offsetTop}px)` }}
+        /*YOU COULD USE THIS FUNCTION METHOD FOR THE INLINE STYLE
+        const boxStyle = {
+          transform: `translateY(${offsetTop}px)`
+        }
+        style={boxStyle}
+        */
+      />
+
+      <button onClick={increment}>+</button>
+      <div>{counter}</div>
+      <button onClick={decrement}>-</button>
     </div>
   );
 }
